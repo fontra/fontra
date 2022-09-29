4948dd44ea8fa205846212dc7c87fdfd4bd8226d
Andrew Graham <andrew.graham@daltonmaag.com>
2022-09-29 08:56:05 +0100

    Remember window state

--- src/fontra/client/main/main.js
+++ src/fontra/client/main/main.js
@@ -1,22 +1,46 @@
-const { app, BrowserWindow } = require('electron')
+const windowStateKeeper = require('electron-window-state');
+
+const { app, BrowserWindow, Menu } = require('electron')
 const path = require('path')
 
 function createWindow () {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    /* webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    } */
-  })
+  const mainWindowState = windowStateKeeper({
+    defaultWidth: 1280,
+    defaultHeight: 800
+  });
+  
+  // Create the browser window.
+    const win = new BrowserWindow({
+      x: mainWindowState.x,
+      y: mainWindowState.y,
+      width: mainWindowState.width,
+      height: mainWindowState.height,
+      minWidth: 512,
+      minHeight: 512,
+      // webPreferences: {
+      //   // Use pluginOptions.nodeIntegration, leave this alone
+      //   // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
+      //   nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
+      //   contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      //   enableRemoteModule: true,
+      //   preload: path.join(__dirname, 'preload.js')
+      // }
+    });
 
-  win.loadFile('src/fontra/client/renderer/landing.html')
+    win.loadFile('src/fontra/client/renderer/landing.html')
+
+    mainWindowState.manage(win);
+    Menu.setApplicationMenu(Menu.buildFromTemplate(getMenubarTemplate(win)));
+  // addWindowListeners(win);
+  
 }
 
 app.whenReady().then(() => {
   createWindow()
 
   app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow()
     }
---
+++ src/fontra/client/main/menubar.js
@@ -0,0 +1,42 @@
+// main/menubar.js
+
+// Function for creating (and recreating) the menubar
+
+export default function getMenubarTemplate(win) {
+    return [
+        {
+        label: "File",
+        submenu: [
+            {
+            // TODO: Add keyboard shortcut
+            label: "Home",
+            click: () => win.loadFile('src/fontra/client/renderer/landing.html')
+            },
+            {
+            // TODO: Add keyboard shortcut
+            label: "Settings",
+            click: () => win.loadFile('src/fontra/client/renderer/settings.html')
+            },
+            { type: "separator" },
+            // {
+            // label: "About dsedit",
+            // click: evt => openAboutDialog(evt, win)
+            // },
+            // { type: "separator" },
+            {
+            label: "Quit",
+            role: "quit"
+            }
+        ]
+        },
+        {
+            label: "View",
+            role: "viewMenu"
+        },
+        {
+        label: "Window",
+        role: "windowMenu"
+        }
+    ];
+}
+  
