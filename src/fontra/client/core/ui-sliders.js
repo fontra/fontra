6ad5acf47f5af0187640a57c3d16052b4be24655
Nedislav Kamburov <nedislav.kamburov@gmail.com>
2023-01-31 16:38:42 +0200

    example formatting of a few different types of files

--- src/fontra/client/core/ui-sliders.js
+++ src/fontra/client/core/ui-sliders.js
@@ -1,5 +1,4 @@
 export class Sliders {
-
   constructor(slidersID, sliderDescriptions) {
     this.container = document.querySelector(`#${slidersID}`);
     this.setSliderDescriptions(sliderDescriptions);
@@ -10,32 +9,32 @@ export class Sliders {
   }
 
   _dispatchListSelectionChanged() {
-    const event = new CustomEvent("slidersChanged", {
-      "bubbles": false,
-      "detail": this,
+    const event = new CustomEvent('slidersChanged', {
+      bubbles: false,
+      detail: this,
     });
     this.container.dispatchEvent(event);
   }
 
   setSliderDescriptions(sliderDescriptions) {
-    this.container.innerHTML = "";  // Delete previous sliders
+    this.container.innerHTML = ''; // Delete previous sliders
     for (const sliderInfo of sliderDescriptions) {
       if (sliderInfo.isDivider) {
-        const divider = document.createElement("hr");
-        divider.className = "slider-divider";
+        const divider = document.createElement('hr');
+        divider.className = 'slider-divider';
         this.container.appendChild(divider);
       } else {
-        const label = document.createElement("label");
-        const slider = document.createElement("input");
-        label.className = "slider-label";
-        slider.type = "range";
-        slider.step = "any";
-        slider.class = "slider";
+        const label = document.createElement('label');
+        const slider = document.createElement('input');
+        label.className = 'slider-label';
+        slider.type = 'range';
+        slider.step = 'any';
+        slider.class = 'slider';
         slider.min = sliderInfo.minValue;
         slider.max = sliderInfo.maxValue;
         slider.value = sliderInfo.defaultValue;
         slider.dataset.name = sliderInfo.name;
-        slider.oninput = event => this._dispatchListSelectionChanged();
+        slider.oninput = (event) => this._dispatchListSelectionChanged();
         label.appendChild(slider);
         label.append(sliderInfo.name);
         this.container.appendChild(label);
