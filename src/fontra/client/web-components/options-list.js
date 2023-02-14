6503bd130ed4f180d5a83a8a58bbc75fb37ce04f
nedkamburov <nedislav.kamburov@gmail.com>
2023-02-14 14:37:12 +0200

    remove all styling from the options-list component

--- src/fontra/client/web-components/options-list.js
+++ src/fontra/client/web-components/options-list.js
@@ -2,86 +2,9 @@ import { html, css, LitElement } from "https://cdn.jsdelivr.net/npm/lit@2.6.1/+e
 
 export class OptionsList extends LitElement {
   static styles = css`
-    .tree {
-      --spacing: 1.5rem;
-      --radius: 10px;
+    ul {
+      list-style: none;
     }
-
-    .tree li {
-      display: block;
-      position: relative;
-      padding-left: calc(2 * var(--spacing) - var(--radius) - 2px);
-    }
-
-    .tree ul {
-      margin-left: calc(var(--radius) - var(--spacing));
-      padding-left: 0;
-    }
-
-    .tree ul li {
-      border-left: 2px solid #ddd;
-    }
-
-    .tree ul li:last-child {
-      border-color: transparent;
-    }
-
-    .tree ul li::before {
-      content: "";
-      display: block;
-      position: absolute;
-      top: calc(var(--spacing) / -2);
-      left: -2px;
-      width: calc(var(--spacing) + 2px);
-      height: calc(var(--spacing) + 1px);
-      border: solid #ddd;
-      border-width: 0 0 2px 2px;
-    }
-
-    .tree summary {
-      display: block;
-      cursor: pointer;
-    }
-
-    .tree summary::marker,
-    .tree summary::-webkit-details-marker {
-      display: none;
-    }
-
-    .tree summary:focus {
-      outline: none;
-    }
-
-    .tree summary:focus-visible {
-      outline: 1px dotted #000;
-    }
-
-    .tree li::after,
-    .tree summary::before {
-      content: "";
-      display: block;
-      position: absolute;
-      top: calc(var(--spacing) / 2 - var(--radius));
-      left: calc(var(--spacing) - var(--radius) - 1px);
-      width: calc(2 * var(--radius));
-      height: calc(2 * var(--radius));
-      border-radius: 50%;
-      background: #ddd;
-    }
-
-    .tree summary::before {
-      content: "+";
-      z-index: 1;
-      background: #222;
-      color: #fff;
-      line-height: calc(2 * var(--radius) - 2px);
-      text-align: center;
-    }
-
-    .tree details[open] > summary::before {
-      content: "−";
-    }
-
     label {
       text-transform: capitalize;
     }
@@ -99,7 +22,7 @@ export class OptionsList extends LitElement {
   optionsList() {
     let listHTML = "";
 
-    if (this.options) {
+    if (this.options.length > 0) {
       listHTML = this.options.map((options) => {
         return html`<li>
           <details .open=${options.defaultOpen}>
@@ -127,7 +50,7 @@ export class OptionsList extends LitElement {
 
   render() {
     return html`
-      <ul class="tree">
+      <ul>
         ${this.optionsList()}
       </ul>
     `;
