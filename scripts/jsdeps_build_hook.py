d3fbfb9e3effbf93b1c6f43546716b52b15ed16a
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-03-10 10:09:28 +0100

    Install npm dependencies in build hook.

--- scripts/jsdeps_build_hook.py
+++ scripts/jsdeps_build_hook.py
@@ -6,4 +6,5 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 class JSDepsBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         super().initialize(version, build_data)
+        subprocess.check_output("npm install", shell=True)
         subprocess.check_output("python -I scripts/jsdeps.py", shell=True)
