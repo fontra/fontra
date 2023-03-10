5682b1ace561810e2e525dddb933e4479f86b15f
Fabio Caccamo <fabio.caccamo@gmail.com>
2023-03-10 10:50:38 +0100

    Convert names from `snake_case` to `camelCase`.

--- scripts/jsdeps.py
+++ scripts/jsdeps.py
@@ -2,37 +2,37 @@ import json
 import shutil
 from pathlib import Path
 
-repo_root = Path(__file__).resolve().parent.parent
-dest_dir = repo_root / "src/fontra/client/third-party/"
+repoRoot = Path(__file__).resolve().parent.parent
+destDir = repoRoot / "src/fontra/client/third-party/"
 
 
-def load_package_dependencies():
-    with open("package.json") as package_file:
-        package_info = json.load(package_file)
-        dependencies = package_info.get("dependencies")
+def loadPackageDependencies():
+    with open("package.json") as packageFile:
+        packageInfo = json.load(packageFile)
+        dependencies = packageInfo.get("dependencies")
         if dependencies:
             return list(dependencies.keys())
     return []
 
 
-def process_dependencies(dependencies):
-    if dest_dir.is_dir():
-        shutil.rmtree(dest_dir)
+def processDependencies(dependencies):
+    if destDir.is_dir():
+        shutil.rmtree(destDir)
     for dependency in dependencies:
-        process_dependency(name=dependency)
+        processDependency(name=dependency)
 
 
-def process_dependency(name):
+def processDependency(name):
     src = Path("node_modules") / name
-    dest = dest_dir / name
+    dest = destDir / name
     dest.parent.mkdir(parents=True, exist_ok=True)
     shutil.copytree(src, dest)
     print(f"[{name}] {str(src)!r} -> {str(dest)!r}")
 
 
 def main():
-    dependencies = load_package_dependencies()
-    process_dependencies(dependencies=dependencies)
+    dependencies = loadPackageDependencies()
+    processDependencies(dependencies=dependencies)
 
 
 if __name__ == "__main__":
