1ac2803e3b08624d1e6beb6492ab39c9df9dc236
Just van Rossum <justvanrossum@gmail.com>
2023-07-16 10:26:19 +0200

    Whitespace

--- scripts/rollup_build_hook.py
+++ scripts/rollup_build_hook.py
@@ -7,5 +7,4 @@ class RollupBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         subprocess.check_output("npm install", shell=True)
         subprocess.check_output("npm run bundle-rollup", shell=True)
-
         build_data["artifacts"].append("/src/fontra/client/third-party/")
