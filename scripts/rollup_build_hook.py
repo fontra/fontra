81ad1daafeee7ebb00f46f5a75d8df95c0eb7004
Just van Rossum <justvanrossum@gmail.com>
2023-07-16 09:37:38 +0200

    Add artifact paths to th build data, perhaps fixing #646

--- scripts/rollup_build_hook.py
+++ scripts/rollup_build_hook.py
@@ -1,3 +1,4 @@
+import pathlib
 import subprocess
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
@@ -7,3 +8,9 @@ class RollupBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
         subprocess.check_output("npm install", shell=True)
         subprocess.check_output("npm run bundle-rollup", shell=True)
+
+        rootDir = pathlib.Path().resolve()
+        thirdPartyPath = rootDir / "src" / "fontra" / "client" / "third-party"
+        for path in thirdPartyPath.iterdir():
+            relPath = path.relative_to(rootDir)
+            build_data["artifacts"].append(f"/{relPath}")
