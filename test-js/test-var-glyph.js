8dd551ca9d96ca289f6e6b6661c5a8677537257a
Olli Meier <hallo@olli-meier.de>
2024-02-21 16:50:23 +0100

    Remove unnecessary

--- test-js/test-var-glyph.js
+++ test-js/test-var-glyph.js
@@ -3,34 +3,6 @@ import { expect } from "chai";
 import { enumerate } from "../src/fontra/client/core/utils.js";
 import { StaticGlyph, VariableGlyph } from "../src/fontra/client/core/var-glyph.js";
 
-const sparseObject = {
-  xAdvance: 170,
-  path: {
-    contourInfo: [{ endPoint: 3, isClosed: true }],
-    coordinates: [60, 0, 110, 0, 110, 120, 60, 120],
-    pointTypes: [0, 0, 0, 0],
-  },
-  components: [
-    {
-      name: "test",
-      location: { a: 0.5 },
-      transformation: {
-        translateX: 0,
-        translateY: 0,
-        rotation: 0,
-        scaleX: 1,
-        scaleY: 1,
-        skewX: 0,
-        skewY: 0,
-        tCenterX: 0,
-        tCenterY: 0,
-      },
-    },
-  ],
-};
-
-const glyph = StaticGlyph.fromObject(sparseObject);
-
 function makeTestGlyphObject() {
   return {
     name: "a",
