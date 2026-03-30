47ae7e3ca1cd345834d595bcd1190ee40dbfde1f
Just van Rossum <justvanrossum@gmail.com>
2026-03-30 16:56:00 +0200

    Add initial tests

---
+++ src-js/fontra-core/tests/test-font-controller.js
@@ -0,0 +1,92 @@
+import { FontController } from "@fontra/core/font-controller.js";
+import { FontraBackend } from "@fontra/core/fontra-backend.js";
+import { LocalFontEngine } from "@fontra/core/local-font-engine.js";
+import { NodePath } from "./node-path.js";
+
+import { expect } from "chai";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const moduleDirName = dirname(fileURLToPath(import.meta.url));
+const mutatorSansPath = join(
+  dirname(dirname(dirname(moduleDirName))),
+  "test-common",
+  "fonts",
+  "MutatorSans.fontra"
+);
+
+async function getFontController(path) {
+  const backend = new FontraBackend(new NodePath(path));
+  await backend.initialize();
+  const engine = new LocalFontEngine(backend);
+  const fontController = new FontController(engine);
+  await fontController.initialize();
+  return fontController;
+}
+
+describe("FontController tests", async () => {
+  it("FontController misc", async () => {
+    const expectedWeightAxis = {
+      name: "weight",
+      label: "weight",
+      tag: "wght",
+      minValue: 100,
+      defaultValue: 100,
+      maxValue: 900,
+      mapping: [
+        [100, 150],
+        [900, 850],
+      ],
+    };
+
+    const fontController = await getFontController(mutatorSansPath);
+    expect(Object.keys(fontController.glyphMap).length).to.equal(55);
+    expect(Object.keys(fontController.characterMap).length).to.equal(74);
+    expect(fontController.glyphMap["A"]).to.deep.equal([65, 97]);
+    expect(fontController.characterMap[65]).to.equal("A");
+    expect(fontController.characterMap[97]).to.equal("A");
+    expect(fontController.axes.mappings).to.deep.equal([]);
+    expect(fontController.axes.axes.length).to.deep.equal(3);
+    expect(fontController.axes.axes[0]).to.deep.equal(expectedWeightAxis);
+    expect(Object.keys(fontController.sources).length).to.equal(8);
+    expect(fontController.sources["light-condensed"].name).to.equal("LightCondensed");
+    expect((await fontController.getFeatures()).text).to.equal("");
+
+    const kerning = await fontController.getKerning();
+    expect(kerning["kern"].sourceIdentifiers).to.deep.equal([
+      "light-condensed",
+      "bold-condensed",
+      "light-wide",
+      "bold-wide",
+      "light-condensed-italic",
+    ]);
+    expect(kerning["kern"].groupsSide1).to.deep.equal({
+      A: ["A", "Aacute", "Adieresis"],
+    });
+    expect(kerning["kern"].groupsSide2).to.deep.equal({
+      A: ["A", "Aacute", "Adieresis"],
+    });
+    expect(kerning["kern"].values["@A"]["V"]).to.deep.equal([
+      -15,
+      null,
+      -180,
+      null,
+      null,
+    ]);
+  });
+
+  it("getGlyphInstance neutral", async () => {
+    const fontController = await getFontController(mutatorSansPath);
+    const glyphController = await fontController.getGlyphInstance("A", {});
+    // expect(Object.keys(glyphController.glyphMap).length).to.equal(11);
+    expect(glyphController.instance.xAdvance).to.equal(396);
+    expect(glyphController.instance.path.coordinates.length).to.equal(32);
+  });
+
+  it("getGlyphInstance interpolation", async () => {
+    const fontController = await getFontController(mutatorSansPath);
+    const glyphController = await fontController.getGlyphInstance("A", { weight: 500 });
+    expect(glyphController.instance.xAdvance).to.equal(568);
+    expect(glyphController.instance.path.coordinates.length).to.equal(32);
+  });
+});
