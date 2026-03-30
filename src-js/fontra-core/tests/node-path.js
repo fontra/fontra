243890b5b958431b0145054133a8dccff9b80fd7
Just van Rossum <justvanrossum@gmail.com>
2026-03-30 15:31:31 +0200

    Add minimal pathlib.Path-like NodePath class, as a filesystem abstraction

---
+++ src-js/fontra-core/tests/node-path.js
@@ -0,0 +1,43 @@
+import fs from "fs";
+import { glob, readFile } from "fs/promises";
+import { basename, dirname, extname, join } from "path";
+
+export class NodePath {
+  constructor(path) {
+    this._path = path;
+  }
+
+  joinpath(...pathsegments) {
+    return new NodePath(join(this._path, ...pathsegments));
+  }
+
+  get parent() {
+    return new NodePath(dirname(this._path));
+  }
+
+  get name() {
+    return basename(this._path);
+  }
+
+  get suffix() {
+    return extname(this._path);
+  }
+
+  get stem() {
+    return basename(this._path).slice(0, -this.suffix.length);
+  }
+
+  async readText() {
+    return await readFile(this._path, { encoding: "utf-8" });
+  }
+
+  async readBytes() {
+    return await readFile(this._path);
+  }
+
+  async *glob(pattern) {
+    for await (const name of glob(pattern, { cwd: this._path })) {
+      yield this.joinpath(name);
+    }
+  }
+}
